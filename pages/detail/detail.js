cde47260dda5a2ec457da15074dc9e2012682038
imsprojo2fan <imsprojo2fan@foxmail.com>
2018-09-06 21:14:36 +0800

    finish comment

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -3,6 +3,11 @@ var app
 var that
 var testUrl = 'http://localhost:7080/wx/resource/list4item'
 var formalUrl = 'https://wx.zooori.cn/wx/resource/list4item'
+var commentUrl = 'https://wx.zooori.cn/wx/comment/list'
+var commentAddUrl = 'https://wx.zooori.cn/wx/comment/add'
+var nickName,email,content;
+var GlobalId = 0;
+var GlobalName;
 Page({
 
   /**
@@ -23,7 +28,10 @@ Page({
     series:[],
     blue:"#6195FF",
     isHide:true,
-    comments:0
+    comments:0,
+    commensArr:[],
+    val:"",
+    hideTip:true
   },
 
   /**
@@ -33,6 +41,8 @@ Page({
     app = getApp()
     that = this
     var id = options.id
+    GlobalId = id;
+    nickName = "" ;email = ""; content="";    
     var dType = options.type
     this.requestData(dType,id)
   },
@@ -96,13 +106,13 @@ Page({
       //header: { 'content-type': 'application/json' },// 默认值
       success: function (res) {
         var data = res.data.data
-        console.log(data)
         //单剧集处理
         var obj = data[0]
           /*设置title*/
           wx.setNavigationBarTitle({
             title: obj.name
           })
+        GlobalName = obj.name;
         var director = obj.director;
         if(director.length>11){
           director = director.substring(0,11)+"..."
@@ -159,6 +169,8 @@ Page({
           isDisplay: 'none',
           isHide:false
         })
+        //获取评论列表
+        that.requestComment(id)
       }
     })
   },
@@ -199,16 +211,263 @@ Page({
       })
     }
   },
-  toResponse: function (e) {
-    var dType = e.type;
-    if (dType == "touchstart") {
-      that.setData({
-        blue: "#fff"
-      })
-    } else {
-      that.setData({
-        blue: "#6195FF"
-      })
+  requestComment: function (id) {
+    wx.request({
+      url: commentUrl,
+      data: {
+        id: id,
+        pageNow:1,
+        pageSize:1000
+      },
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      //header: { 'content-type': 'application/json' },// 默认值
+      success: function (res) {
+        var data = res.data.data
+        for(var i=0;i<data.length;i++){
+          var nickName = data[i].nickName;
+          if(nickName.length>10){
+            data[i].nickName = nickName.substring(0,10)+"...";
+          }
+          var time = data[i].created;
+          data[i].created = that.getDateDiff(time);
+        }
+        that.setData({
+          comments:data.length,
+          commentArr:data
+        })
+        if(data.length==0){
+          that.setData({
+            hideTip:false
+          })
+        }else{
+          that.setData({
+            hideTip: true
+          })
+        }
+       
+      },
+      fail: function () {
+
+      },
+      complete: function () {
+        that.setData({
+          isDisplay: 'none',
+          isHide: false
+        })
+      }
+    })
+  },
+  getDateDiff:function(dateStr){
+    var publishTime = that.getDateTimeStamp(dateStr) / 1000,
+      d_seconds,
+      d_minutes,
+      d_hours,
+      d_days,
+      timeNow = parseInt(new Date().getTime() / 1000),
+      d,
+
+      date = new Date(publishTime * 1000),
+      Y = date.getFullYear(),
+      M = date.getMonth() + 1,
+      D = date.getDate(),
+      H = date.getHours(),
+      m = date.getMinutes(),
+      s = date.getSeconds();
+    //小于10的在前面补0
+    if (M < 10) {
+      M = '0' + M;
+    }
+    if (D < 10) {
+      D = '0' + D;
+    }
+    if (H < 10) {
+      H = '0' + H;
+    }
+    if (m < 10) {
+      m = '0' + m;
+    }
+    if (s < 10) {
+      s = '0' + s;
+    }
+
+    d = timeNow - publishTime;
+    d_days = parseInt(d / 86400);
+    d_hours = parseInt(d / 3600);
+    d_minutes = parseInt(d / 60);
+    d_seconds = parseInt(d);
+
+    if (d_days > 0 && d_days < 3) {
+      return d_days + '天前';
+    } else if (d_days <= 0 && d_hours > 0) {
+      return d_hours + '小时前';
+    } else if (d_hours <= 0 && d_minutes > 0) {
+      return d_minutes + '分钟前';
+    } else if (d_seconds < 60) {
+      if (d_seconds <= 0) {
+        return '刚刚发表';
+      } else {
+        return d_seconds + '秒前';
+      }
+    } else if (d_days >= 3 && d_days < 30) {
+      return M + '-' + D + '&nbsp;' + H + ':' + m;
+    } else if (d_days >= 30) {
+      return Y + '-' + M + '-' + D + '&nbsp;' + H + ':' + m;
     }
   },
+  getDateTimeStamp: function (dateStr){
+    return Date.parse(dateStr.replace(/-/gi, "/"));
+  },
+  inputVal:function(e){
+    var dType = e.currentTarget.dataset.type;
+    var val = e.detail.value;
+    if(dType=="nickName"){
+      nickName = val.trim();
+    }else if(dType=="email"){
+      email = val.trim();
+    }else{
+      content = val.trim();
+    }
+  },
+  submit:function(e){
+    if(!nickName){
+      wx.showModal({
+        title: '提示',
+        content: '我们好像很想知道您的称谓',
+        showCancel:false,
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          }
+        }
+      })
+      return
+    }
+    if (!email) {
+      wx.showModal({
+        title: '提示',
+        content: '邮箱不能为空喔!',
+        showCancel: false,
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          }
+        }
+      })
+      return
+    }
+
+    var reg = new RegExp("^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$");
+    if (!reg.test(email)) {
+      wx.showModal({
+        title: '提示',
+        content: '邮箱格式貌似不正确呢!',
+        showCancel: false,
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          }
+        }
+      })
+      return;
+    }
+    if(!content){
+      wx.showModal({
+        title: '提示',
+        content: '我猜您肯定忘了写一些感想!',
+        showCancel: false,
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          }
+        }
+      })
+      return;
+    }
+
+    that.setData({
+      isDisplay: 'block',
+      isHide: true
+    })
+
+    wx.request({
+      url: commentAddUrl,
+      data: {
+        rid: GlobalId,
+        nickName: nickName,
+        email: email,
+        content:content,
+        count:that.data.comments
+      },
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      //header: { 'content-type': 'application/json' },// 默认值
+      success: function (res) {
+        var data = res.data.data
+        if(res.data.code==1){
+          that.setData({
+            val:""
+          })
+          that.requestComment(GlobalId,1,1000);
+        }
+      },
+      fail: function () {
+
+      },
+      complete: function () {
+        that.setData({
+          isDisplay: 'none',
+          isHide: false
+        })
+      }
+    })
+  },
+  toResponse:function(){
+    wx.showModal({
+      title: '提示',
+      content: '貌似是资源不可播放了是吗?',
+      showCancel: true,
+      success: function (res) {
+        if (res.confirm) {
+
+          that.setData({
+            isDisplay: 'block',
+            isHide: true
+          })
+
+          wx.request({
+            url: "https://wx.zooori.cn/wx/resource/report",
+            data: {
+              id: GlobalId,
+              name: GlobalName
+            },
+            method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+            //header: { 'content-type': 'application/json' },// 默认值
+            success: function (res) {
+              var data = res.data.data
+              if (res.data.code == 1) {
+                wx.showModal({
+                  title: '提示',
+                  content: '小哥哥将尽快处理问题呢 /比❤',
+                  showCancel: false,
+                  success: function (res) {
+                    if (res.confirm) {
+                      console.log('用户点击确定')
+                    }
+                  }
+                })
+              }
+            },
+            fail: function () {
+
+            },
+            complete: function () {
+              that.setData({
+                isDisplay: 'none',
+                isHide: false
+              })
+            }
+          })
+        }
+      }
+    })
+  }
 })
